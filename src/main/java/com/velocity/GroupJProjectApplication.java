a5084ec95d7d1fb6f6a58244cf4a8bf1f4d32730
vishal <vishalgwaghmare31@gmail.com>
2023-05-15 22:55:47 +0530

    This is First commit....

---
+++ src/main/java/com/velocity/GroupJProjectApplication.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupJProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupJProjectApplication.class, args);
+	}
+
+}
