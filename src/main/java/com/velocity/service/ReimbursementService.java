ad9f32f836b9a4d54df21d3e195922fd445e12d0
Madhurimagaikwad <madhurimag1996@gmail.com>
2023-05-16 13:07:13 +0530

    Reimbursement(controller,Repository,service, serviceImpl) User(Repository,model,service) Added

---
+++ src/main/java/com/velocity/service/ReimbursementService.java
@@ -0,0 +1,14 @@
+package com.velocity.service;
+
+import java.util.List;
+
+import com.velocity.model.Reimbursement;
+
+public interface ReimbursementService {
+
+	public Reimbursement saveReimbursement(Reimbursement reimbursement);
+	public Reimbursement updateReimbursement(Reimbursement reimbursement);
+	public List<Reimbursement> getReimbursement();
+	public void deletById(int reimbursementId);
+	
+}
---
+++ src/main/java/com/velocity/service/UserService.java
@@ -0,0 +1,8 @@
+package com.velocity.service;
+
+import com.velocity.model.User;
+
+public interface UserService {
+
+	public User updateUser(User user);
+}
