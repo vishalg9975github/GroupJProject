eddb44b7655f6432ce71fdb3e4267113d2b634a0
vishalg9975github <130144958+vishalg9975github@users.noreply.github.com>
2023-05-18 10:02:47 +0530

    Update CurrencyConversionBeanController.java

--- src/main/java/com/velocity/controller/CurrencyConversionBeanController.java
+++ src/main/java/com/velocity/controller/CurrencyConversionBeanController.java
@@ -14,7 +14,6 @@ import com.velocity.service.CurrencyConversionBeanService;
 public class CurrencyConversionBeanController {
 	
 	//inject service 
-	
 	@Autowired
 	private CurrencyConversionBeanService currencyConversionBeanService;
 	
