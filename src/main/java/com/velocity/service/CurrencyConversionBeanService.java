6a3244f78c15dd4ec5763dfdb16bc7b5fe1148b2
vishalg9975github <130144958+vishalg9975github@users.noreply.github.com>
2023-05-18 09:55:52 +0530

    Update CurrencyConversionBeanService.java

--- src/main/java/com/velocity/service/CurrencyConversionBeanService.java
+++ src/main/java/com/velocity/service/CurrencyConversionBeanService.java
@@ -4,8 +4,8 @@ import com.velocity.model.CurrencyConversionBean;
 
 public interface CurrencyConversionBeanService {
 	
+	/*Author Anushka */
 	public CurrencyConversionBean updateCurrencyConversionBean(CurrencyConversionBean currencyConversionBean);
-
 	/* Author Madhurima */
 	public void deleteById(Long id);
 	/* Author Vishal */
